315bc942e229ed61683fceb2caf6c88a8ecf40f0
Murilo Ramos <okamael.adc@gmail.com>
2022-08-23 21:56:16 -0300

    Configuração gerais

---
+++ src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -0,0 +1,45 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.movieflix.entities.Review;
+
+public class ReviewDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6290975008778257139L;
+	private String text;
+	private Long movieId;
+	
+	public ReviewDTO() {
+		
+	}
+	
+	public ReviewDTO(String text, Long movieId) {
+		this.text = text;
+		this.movieId = movieId;
+	}
+	
+	public ReviewDTO(Review entity) {
+		
+		this.text = entity.getText();
+		this.movieId = entity.getMovie().getId();
+	}
+
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Long getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+	
+	
+}
